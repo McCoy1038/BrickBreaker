f60be430d7ba15e809cd78c66b84a4e83bd691f2
Sam Faw <sfaw@lr1038.local>
2022-10-22 11:27:49 -0400

    testing testing 1 2 3

--- MapGenerator.java
+++ MapGenerator.java
@@ -1,11 +1,21 @@
+import java.awt.Graphics2D;
+
 public class MapGenerator {
+
+    public int map[][];
     
     public MapGenerator(){
 
     }
     
     public MapGenerator(int row, int col){
-     
+        map = new int[row][col];
+        
     }
 
+    public void draw(Graphics2D graphic) {
+        for(int i = 0; i < map.length; i++) {
+
+        }
+    }
 }
