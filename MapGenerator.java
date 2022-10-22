75cd52479b0a117efafd7e51819e73618fead43d
McCoy Adkins <madkins@lr1038.local>
2022-10-22 11:10:01 -0400

    draw method

--- MapGenerator.java
+++ MapGenerator.java
@@ -1,11 +1,23 @@
+import java.awt.Graphics2D;
+
 public class MapGenerator {
     
+    public int map[][];
+
     public MapGenerator(){
 
     }
     
     public MapGenerator(int row, int col){
      
+        map = new int[row][col];
+
     }
 
+    public void draw(Graphics2D graphics){
+     
+        for(int i=0; i<map.length; i++){
+            
+        }
+    }
 }
