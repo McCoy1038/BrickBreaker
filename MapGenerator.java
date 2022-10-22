d9cf1c4b92b890b9b16ef5a9823a76f3ed1ac0fb
Sam Faw <sfaw@lr1038.local>
2022-10-22 11:44:56 -0400

    updated MapGenerator constructor

--- MapGenerator.java
+++ MapGenerator.java
@@ -4,18 +4,22 @@ public class MapGenerator {
 
     public int map[][];
 
-    public MapGenerator(){
+    public MapGenerator() {
         
     }
     
-    public MapGenerator(int row, int col){
-     
+    public MapGenerator(int row, int col) {
         map = new int[row][col];
         
+        for(int i = 0; i < map.length; i++) {
+            for(int j = 0; j < map[0].length; j++) {
+                map[i][j] = 1;
+            }
+        }
     }
 
     public void draw(Graphics2D graphic){
-        for (int i = 0; i < map.length; i++){
+        for (int i = 0; i < map.length; i++) {
 
         }
     }
