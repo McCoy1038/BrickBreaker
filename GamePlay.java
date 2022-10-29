ead4be7d53bc4f79ff5a5c8a8d356398440d2d3a
Paige Lantman <plantman@lr1038.local>
2022-10-29 10:24:50 -0400

    finished paint method

--- GamePlay.java
+++ GamePlay.java
@@ -6,10 +6,19 @@ import java.awt.event.ActionEvent;
 import javax.swing.Timer;
 import java.awt.Graphics;
 import java.awt.Color;
+import java.awt.Font;
 import java.awt.Graphics2D;
 
 public class GamePlay extends JPanel implements KeyListener, ActionListener {
     
+    private int score = 0;
+    private int totalBricks = 48;
+    private int playerX = 310;
+    private int ballPosX = 120;
+    private int ballPosY = 350;
+    private int ballXdir = -1;
+    private int ballYdir = -2;
+    private boolean play = false;
     private MapGenerator map; 
     private Timer timer;
     private int delay = 8;
@@ -32,6 +41,55 @@ public class GamePlay extends JPanel implements KeyListener, ActionListener {
         
         // Drawing map
         map.draw((Graphics2D) g);
+
+        //Setting Borders
+        g.setColor(Color.cyan);
+        g.fillRect(0, 0, 3, 592);
+        g.fillRect(0, 0, 692, 3);
+        g.fillRect(691, 0, 3, 592);
+
+        //Scores
+        g.setColor(Color.white);
+        g.setFont(new Font("serif", Font.BOLD, 25));
+        g.drawString("" + score, 590, 30);
+
+        //Paddle
+        g.setColor(Color.cyan);
+        g.fillRect(playerX, 550, 100, 8);
+    
+        //The Ball
+        g.setColor(Color.green);
+        g.fillOval(ballPosX, ballPosY, 20, 20);
+
+        if(totalBricks <= 0){
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 30));
+            g.drawString("You Won!", 260, 300);
+
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Press (Enter) to Restart", 230, 350);
+        }
+
+        if(ballPosY > 570){
+            play = false;
+            ballXdir = 0;
+            ballYdir = 0;
+
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 30));
+            g.drawString("Game Over, Scores: " + score, 190, 300);
+
+            g.setColor(Color.red);
+            g.setFont(new Font("serif", Font.BOLD, 20));
+            g.drawString("Press (Enter) to Restart", 230, 350);
+        }
+
+        g.dispose();
     }
 
     @Override
